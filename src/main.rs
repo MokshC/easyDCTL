2c0029b049bed760a7753be1f875ffcf434126e2
MokshC <mokshchitkara@gmail.com>
2024-06-28 18:16:37 -0700

    removed web api

--- src/main.rs
+++ src/main.rs
@@ -23,38 +23,3 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Box::new(easydctl::TemplateApp::new(cc))),
     )
 }
-
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        let start_result = eframe::WebRunner::new()
-            .start(
-                "the_canvas_id", // hardcode it
-                web_options,
-                Box::new(|cc| Box::new(easydctl::TemplateApp::new(cc))),
-            )
-            .await;
-        let loading_text = web_sys::window()
-            .and_then(|w| w.document())
-            .and_then(|d| d.get_element_by_id("loading_text"));
-        match start_result {
-            Ok(_) => {
-                loading_text.map(|e| e.remove());
-            }
-            Err(e) => {
-                loading_text.map(|e| {
-                    e.set_inner_html(
-                        "<p> The app has crashed. See the developer console for details. </p>",
-                    )
-                });
-                panic!("failed to start eframe: {e:?}");
-            }
-        }
-    });
-}
